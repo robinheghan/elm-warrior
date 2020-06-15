c28d6f48a51c945ab9617c8f62b9c91310581614
Phill Sparks <me@phills.me.uk>
2020-06-15 08:35:20 +0100

    Add Tests workflow
    - Run elm-test
    - Confirm elm docs build

---
+++ tests/Tests/Warrior.elm
@@ -0,0 +1,15 @@
+module Tests.Warrior exposing (all)
+
+import Expect
+import Test exposing (Test, describe, test)
+import Warrior
+
+
+all : Test
+all =
+    describe "Warrior"
+        -- TODO: Add Warrior tests
+        [ test "package compiles" <|
+            \() ->
+                Expect.pass
+        ]
